aa400b759b23d43f4f70e91418602598c7a80b4b
Tim Seckinger <seckinger.tim@gmail.com>
2019-11-12 14:44:33 +0000

    emotion

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import css from "@emotion/css/macro";
+import logo from "./logo.svg";
+
+const headerStyles = css`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div
+      css={css`
+        text-align: center;
+      `}
+    >
+      <header css={headerStyles}>
+        <img src={logo} alt="logo" css={{ height: "40vmin" }} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          css={{ color: "#09d3ac" }}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
